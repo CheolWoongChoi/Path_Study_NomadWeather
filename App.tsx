33dd367f45bef514880c91d1f188ec07e661d0ed
CheolWoong Choi <bluesky4381@naver.com>
2024-01-24 22:59:08 +0900

    feat : 날씨앱 스타일 적용

--- App.tsx
+++ App.tsx
@@ -1,26 +1,52 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is Path RN Study```</Text>
-      <Text style={styles.text}>Hello, World!</Text>
-      <StatusBar style="inverted" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
+// Colors
+// CDF7F6, 8FB8DE, 9A94BC
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "skyblue",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#CDF7F6",
   },
-  text: {
-    color: "white",
-    fontWeight: "bold",
-    fontSize: 40,
+  city: {
+    flex: 1,
+    backgroundColor: "#8FB8DE",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    backgroundColor: "#9A94BC",
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
   },
 });
